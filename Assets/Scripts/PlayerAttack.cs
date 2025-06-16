f5ff7efc7bc6aec5aa9688e402d9bd082c136602
フカシギG <kouyoubokun@gmail.com>
2025-06-17 00:40:35 +0900

    unitask導入、プレイヤー攻撃の実装はじめ（敵の探索ができた）

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -1,3 +1,4 @@
+using Cysharp.Threading.Tasks;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +7,51 @@ public class PlayerAttack : MonoBehaviour
 {
     [SerializeField] GameObject bullet;
 
-    [SerializeField] Collider2D ATcollider;
+    [SerializeField] float detectionRadius;
+    [SerializeField] float coolTime;
+
+    [SerializeField] LayerMask targetLayer;
+
+    bool a;
 
     void Start()
     {
+
+    }
+
+    async void AttackTask()
+    {
+        while (true)
+        {
+            await UniTask.WaitUntil(() => a == true);
+
+            ShooteBullet();
+
+            await UniTask.Delay((int)(coolTime * 1000));
+        }
+    }
+
+    GameObject FindEnemy()
+    {
+
+        //ˆê’è”ÍˆÍ“à‚Ì“G‚ğ”z—ñ‚ÉŠi”[
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, detectionRadius, targetLayer);
+
+        GameObject nearestObject = null;
+        float shortestDistance = Mathf.Infinity; // –³ŒÀ‘å‚Å‰Šú‰»
+
+        foreach (Collider2D hit in hits)
+        {
+            float Distance = Vector2.Distance(transform.position, hit.transform.position);
+
+            if(Distance < shortestDistance )
+            {
+                shortestDistance = Distance;
+                nearestObject = hit.gameObject;
+            }
+        }
+
+        return nearestObject;
     }
 
     void ShooteBullet()
