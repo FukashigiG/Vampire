c0633070e601f1239fb0bb0a1d8486699ef5afe6
fukashigi <kouyoubokun@gmail.com>
2025-11-02 15:34:03 +0900

    HyperHSpEを修正

---
+++ Assets/Scripts/ScriptableObj/Treasure/BoostStatusEffectTreasure.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewTreasure", menuName = "Game Data/Treasure Data/BoostStatusEffect")]
+public class BoostStatusEffectTreasure : Base_TreasureData
+{
+    // ŠŽ‚µ‚Ä‚¢‚éŠÔA“G‚©Ž©•ª‚ªŽó‚¯‚éó‘Ô•Ï‰»‚ÌŒø‰Ê‚ð‘Œ¸
+
+    // ‘ÎÛ‚Í“G‚©Ž©•ª‚©
+    [SerializeField] bool isTargetEnemy;
+    // ‘ÎÛ‚Ìó‘Ô•Ï‰»
+    [SerializeField] StatusEffectType targetEffectType;
+    // ‘Œ¸‚³‚¹‚éŒø‰ÊŽžŠÔ—Ê
+    [SerializeField] float amount_ExtraDuration;
+    [SerializeField] int amount_;
+
+    public override void OnAdd(PlayerStatus status)
+    {
+
+    }
+
+    public override void OnRemove(PlayerStatus status)
+    {
+
+    }
+
+    public override void SubscribeToEvent(PlayerStatus status, CompositeDisposable disposables)
+    {
+        switch (isTargetEnemy)
+        {
+            case true:
+
+                EnemyStatus.onGetStatusEffect.Subscribe(c =>
+                {
+                    if (c.type == targetEffectType) c.duration += amount_ExtraDuration;
+
+                }).AddTo(disposables);
+
+                break;
+
+            case false:
+
+                PlayerStatus.onGetStatusEffect.Subscribe(c =>
+                {
+                    if (c.type == targetEffectType) c.duration += amount_ExtraDuration;
+
+                }).AddTo(disposables);
+
+                break;
+        }
+
+
+    }
+}
--- Assets/Scripts/ScriptableObj/Treasure/HyperHSpETreasure.cs
+++ Assets/Scripts/ScriptableObj/Treasure/HyperHSpETreasure.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UniRx;
 using UnityEngine;
+using System.Linq;
 
 [CreateAssetMenu(fileName = "NewTreasure", menuName = "Game Data/Treasure Data/HyperHSpE")]
 public class HyperHSpETreasure : Base_TreasureData
@@ -9,6 +10,8 @@ public class HyperHSpETreasure : Base_TreasureData
     // 所持している間、プレイヤーの扱うナイフの特定の特殊能力を強化
 
     [field: SerializeField] public BaseHSpE hspe { get; private set; }
+
+    System.Type targetType => hspe.GetType();
 
     public override void OnAdd(PlayerStatus status)
     {
@@ -24,7 +27,14 @@ public class HyperHSpETreasure : Base_TreasureData
     {
         status.attack.onThrowKnife.Subscribe(_knifeData =>
         {
-            if(! _knifeData.specialEffects.Contains(hspe)) return;
+            // 対象のHSpEがあれば、それを取得
+            BaseHSpE matchedEffect = _knifeData.specialEffects
+                .FirstOrDefault(effect => effect.GetType() == targetType);
+
+            if (matchedEffect != null)
+            {
+
+            }
         })
         .AddTo(disposables);
     }
