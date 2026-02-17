dd9f31aecd207e8ced7ed04ea941f8936e4dbb47
フカシギG <kouyoubokun@gmail.com>
2026-02-18 00:04:16 +0900

    青用の必殺技の効果を改修

--- Assets/Scripts/ScriptableObj/P_CharaAbility/PCA_BigMagic.cs
+++ Assets/Scripts/ScriptableObj/P_CharaAbility/PCA_BigMagic.cs
@@ -13,18 +13,49 @@ public class PCA_BigMagic : Base_P_CharaAbility
     // 複数体召喚し、周囲の敵全てにダメージと凍結効果
 
     [SerializeField] int num_SummonSpilit;
+    [SerializeField] int damagePoint;
 
     [SerializeField] GameObject spirit_Prefab;
 
+    [SerializeField] Base_StatusEffectData statusEffect;
+
+    [SerializeField] LayerMask spiritLayer;
+    [SerializeField] LayerMask enemyLayer;
+
+    [SerializeField] GameObject fx_Charge;
+    [SerializeField] GameObject fx_Impact;
+
     async public override UniTask ActivateAbility(CancellationToken token)
     {
+        Vector2 playerPos = PlayerController.Instance.transform.position;
+
         for (int i = 0;i < num_SummonSpilit; i++)
         {
-            Instantiate(spirit_Prefab, PlayerController.Instance.transform.position, Quaternion.identity);
+            Instantiate(spirit_Prefab, playerPos + new Vector2(Random.Range(-3f, 3f), Random.Range(-3f, 3f)), Quaternion.identity);
         }
 
+        Instantiate(fx_Charge, playerPos, Quaternion.identity, PlayerController.Instance.transform);
 
+        await UniTask.Delay((int)(1000), cancellationToken: token);
 
-        await UniTask.Delay((int)(600), cancellationToken: token);
+        playerPos = PlayerController.Instance.transform.position;
+
+        Instantiate(fx_Impact, playerPos, Quaternion.identity);
+
+        Collider2D[] spirits = Physics2D.OverlapCircleAll(playerPos, 3, spiritLayer);
+
+        float radius = spirits.Length * 2.5f;
+
+        Collider2D[] hits = Physics2D.OverlapCircleAll(playerPos, radius, enemyLayer);
+
+        foreach (Collider2D hit in hits)
+        {
+            if (hit.TryGetComponent(out Base_MobStatus ms))
+            {
+                ms.GetAttack(0, damagePoint, playerPos);
+
+                ms.ApplyStatusEffect(statusEffect, "BigMagicEffect", spirits.Length * 0.4f, 0);
+            }
+        }
     }
 }
