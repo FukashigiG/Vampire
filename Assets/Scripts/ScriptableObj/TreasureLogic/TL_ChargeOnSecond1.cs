68ff9fba26d1bf8224b9449045a2562cb28fd28b
フカシギG <kouyoubokun@gmail.com>
2026-03-02 17:40:28 +0900

    奇妙系秘宝の仕様変更、回復瓶の回復量を5現象

---
+++ Assets/Scripts/ScriptableObj/TreasureLogic/TL_ChargeOnSecond1.cs
@@ -0,0 +1,24 @@
+﻿using UniRx;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewTreasure", menuName = "Game Data/TreasureLogic/ChargeOnReload")]
+public class TL_ChargeOnReload : Base_TreasureLogic
+{
+    // HPがN割以上なら、リロード時必殺技ゲージをNチャージする
+
+    [SerializeField] int chargeAmount;
+    [SerializeField] int border_HP_Percent;
+
+    public override void SubscribeToEvent(PlayerStatus status, CompositeDisposable disposables)
+    {
+        status.attack.onReload.Subscribe(x =>
+        {
+            float ratio_HP = (float)status.hitPoint.Value / (float)status.maxHP * 100f;
+
+            if (ratio_HP < border_HP_Percent) return;
+
+            status.attack.AbilityCharge(chargeAmount); 
+
+        }).AddTo(disposables);
+    }
+}
